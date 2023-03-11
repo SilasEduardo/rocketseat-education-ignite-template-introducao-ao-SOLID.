8d3c1c7823873e3b2509e1008fbb02e0a897acc6
SilasEduardo <silasandrade94@gmail.com>
2023-03-11 19:53:06 -0300

    update

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -13,7 +13,6 @@ class ListAllUsersController {
       });
       return response.json(all);
     } catch (err) {
-      console.log(err);
       return response.status(400).json({ error: err });
     }
   }
