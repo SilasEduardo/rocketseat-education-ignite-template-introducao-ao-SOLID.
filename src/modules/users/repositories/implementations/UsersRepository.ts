bbc2ff2b0212c7e78b0486b7aca4c7a653331b3a
SilasEduardo <silasandrade94@gmail.com>
2023-03-11 17:22:06 -0300

    node user repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,41 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      admin: false,
+      email,
+      created_at: Date,
+      updated_at: Date,
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const exitsUser = this.users.find((user) => user.id === id);
+    return exitsUser;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const exitsUser = this.users.find((user) => user.email === email);
+    return exitsUser;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    Object.assign(receivedUser, {
+      admin: true,
+      updated_at: new Date(),
+    });
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.list();
   }
 }
 
